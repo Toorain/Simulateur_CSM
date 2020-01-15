82961630f54eb7d2b940f480176bef0ec58c9156
WILMES <maxencewilmes@gmail.com>
2020-01-15 15:29:41 +0100

    v 0.1.2
    Save before integration

--- src/js/enregistrement.js
+++ src/js/enregistrement.js
@@ -20,24 +20,24 @@ document.getElementById("send").addEventListener("click", () => {
   versement = document.getElementById("ETSVersement").value;
   radio = document.getElementsByName("customRadio");
 
-  for (var i = 0; i < radio.length; i++) {
-    if (radio[i].checked) {
-      // do whatever you want with the checked radio
-      moyenDePaiement = radio[i].value;
+  // for (var i = 0; i < radio.length; i++) {
+  //   if (radio[i].checked) {
+  //     // do whatever you want with the checked radio
+  //     moyenDePaiement = radio[i].value;
 
-      // only one radio can be logically checked, don't check the rest
-      break;
-    }
-  }
+  //     // only one radio can be logically checked, don't check the rest
+  //     break;
+  //   }
+  // }
 
   // We check if all the fields are filled.
-  if (
-    rs !== "" &&
-    siret !== "" &&
-    mail !== "" &&
-    tel !== "" &&
-    versement !== ""
-  ) {
+  // if (
+  //   rs !== "" &&
+  //   siret !== "" &&
+  //   mail !== "" &&
+  //   tel !== "" &&
+  //   versement !== ""
+  // ) {
     if (parseInt(versement) > 0 && parseInt(versement) <= parseFloat(ETSValueP2)) {
       $("#formElement :input").prop("disabled", true);
       document.getElementById("getPDF").removeAttribute("disabled");
@@ -46,49 +46,72 @@ document.getElementById("send").addEventListener("click", () => {
       document.getElementById('passwordHelp').className = 'text-danger isHidden';
       var url = "/Simulateur_CSM/src/TestBook.xlsx";
 
-      /* set up async GET request */
-      var req = new XMLHttpRequest();
-      req.open("GET", url, true);
-      req.responseType = "arraybuffer";
+      // /* set up async GET request */
+      // var req = new XMLHttpRequest();
+      // req.open("GET", url, true);
+      // req.responseType = "arraybuffer";
 
-      req.onload = function(e) {
-        var data = new Uint8Array(req.response);
-        var wb = XLSX.read(data, { type: "array" });
-        var ws = wb.Sheets.Sheet1;
-        XLSX.utils.sheet_add_aoa(ws, [["new data", 1, 2, 3]], { origin: -1 });
+      // req.onload = function(e) {
+      //   var data = new Uint8Array(req.response);
+      //   var wb = XLSX.read(data, { type: "array" });
+      //   var ws = wb.Sheets.Sheet1;
+      //   XLSX.utils.sheet_add_aoa(ws, [["new data", 1, 2, 3]], { origin: -1 });
 
-        XLSX.writeFile(wb, "new.xlsx");
+      //   XLSX.writeFile(wb, "new.xlsx");
 
-        // var htmlstr = XLSX.write(wb,{sheet:"Sheet1", type:'binary',bookType:'html'});
-        // $('#wrapper')[0].innerHTML += htmlstr;
-      };
+      //   // var htmlstr = XLSX.write(wb,{sheet:"Sheet1", type:'binary',bookType:'html'});
+      //   // $('#wrapper')[0].innerHTML += htmlstr;
+      // };
 
-      req.send();
+      // req.send();
     } else {
       document.getElementById('passwordHelp').innerHTML = 'Doit être compris entre 1 et ' + ETSValueP2;
       document.getElementById('passwordHelp').className = 'text-danger';
     }
-  }
+  // }
 });
 
 document.getElementById("getPDF").addEventListener("click", () => {
-  var height = 40;
-  // Default export is a4 paper, portrait, using milimeters for units
-  var doc = new jsPDF();
+  var height = 60;
+  var interligne = 15;
+  // change non-opaque pixels to white
 
-  doc.text('Nom de l\'entreprise :', 80, height, "center");
-  doc.text(rs, 120, height, "center");
-  doc.text('Numéro de Siret :', 80, height += 10, "center");
-  doc.text(siret, 120, height, "center");
-  doc.text(ETSValueP2 + ' €', 105, height += 30, "center");
-  doc.text("à verser par", 105, height += 10, "center");
-  doc.text(moyenDePaiement, 105, height += 10, "center");
-  doc.text("à l'adresse", 105, height += 10, "center");
-  doc.text("Etablissement ", 105, height += 10, "center");
-  doc.text("Adresse numéro de la rue", 105, height += 10, "center");
-  doc.text("Ville et code postal", 105, height += 10, "center");
+  // Default export is a4 paper, portrait, using milimeters for units
+  //var doc = new jsPDF();
+
+  function getImgFromUrl(logo_url, callback) {
+      var img = new Image();
+      img.src = logo_url;
+      img.onload = function () {
+          callback(img);
+      };
+  } 
+
+  function generatePDF(img){
+    var doc = new jsPDF();
+    doc.addImage(img, 'JPEG', 1, 2, 210, 30);
+    doc.setFontSize(24);
+    doc.text('Nom de l\'entreprise :', 105, height, "center");
+    doc.text(rs, 105, height += interligne, "center");
+    doc.text('Numéro de Siret :', 105, height += 20, "center");
+    doc.text(siret, 105, height += interligne, "center");
+    doc.setFontSize(20);
+    doc.text('Le montant à verser est de : ' + ETSValueP2, 105, height += 50, "center");
+    doc.text("A verser par : " + moyenDePaiement, 105, height += interligne, "center");
+    doc.text("A l'adresse", 105, height += interligne, "center");
+    doc.setFontSize(18);
+    doc.text("Etablissement ", 105, height += interligne, "center");
+    doc.text("Adresse numéro de la rue", 105, height += interligne, "center");
+    doc.text("Ville et code postal", 105, height += interligne, "center");
+    doc.save("a4.pdf");
+  }
+
+  var logo_url = "http://localhost:8888/Simulateur_CSM/src/assets/img/bannerCSM.jpeg";
+  getImgFromUrl(logo_url, function (img) {
+      generatePDF(img);
+  });
   // doc.autoTable({html: document.getElementById('pdfJS')});  //as simple as that!
-  doc.save("a4.pdf");
+  
 });
 
 document.getElementById("reloadForm").addEventListener("click", () => {
