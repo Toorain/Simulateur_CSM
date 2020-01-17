0a6c218850ca31b1371772f955be4d9b6f1a4bd7
WILMES <maxencewilmes@gmail.com>
2020-01-17 11:56:16 +0100

    v 0.2.2 Fonts / Form
    - Ajout des fonts
    - Amélioration du formulaire
    - Amélioration de la génération du PDF

--- src/js/calcul.js
+++ src/js/calcul.js
@@ -87,6 +87,7 @@ document.getElementById("send").addEventListener("click", () => {
   } else if (radioPartiel) {
     montant = document.getElementById('inputPartiel').value;
   }
+  montantDu = document.getElementById('contributionETS').value;
   rs = document.getElementById('validationSociale').value;
   siret = document.getElementById('validationSiret').value;
   mail = document.getElementById('validationMail').value;
@@ -98,108 +99,83 @@ document.getElementById("send").addEventListener("click", () => {
   } else if (virement) {
     moyenDePaiement = 'RIB';
   } 
-  
-
-  // for (var i = 0; i < radio.length; i++) {
-  //   if (radio[i].checked) {
-  //     // do whatever you want with the checked radio
-  //     moyenDePaiement = radio[i].value;
-
-  //     // only one radio can be logically checked, don't check the rest
-  //     break;
-  //   }
-  // }
-
   // We check if all the fields are filled.
-  // if (
-  //   rs !== "" &&
-  //   siret !== "" &&
-  //   mail !== "" &&
-  //   tel !== "" &&
-  //   versement !== ""
-  // ) {
-    // if (parseInt(versement) > 0 && parseInt(versement) <= parseFloat(ETSValue)) {
-    //   var url = "/Simulateur_CSM/src/TestBook.xlsx";
-
-    //   /* set up async GET request */
-    //   var req = new XMLHttpRequest();
-    //   req.open("GET", url, true);
-    //   req.responseType = "arraybuffer";
-
-    //   req.onload = function(e) {
-    //     var data = new Uint8Array(req.response);
-    //     var wb = XLSX.read(data, { type: "array" });
-    //     var ws = wb.Sheets.Sheet1;
-    //     XLSX.utils.sheet_add_aoa(ws, [["new data", 1, 2, 3]], { origin: -1 });
-
-    //     XLSX.writeFile(wb, "new.xlsx");
-
-    //     // var htmlstr = XLSX.write(wb,{sheet:"Sheet1", type:'binary',bookType:'html'});
-    //     // $('#wrapper')[0].innerHTML += htmlstr;
-    //   };
-
-    //   req.send();
-    // } else {
-    //   document.getElementById('passwordHelp').innerHTML = 'Doit être compris entre 1 et ' + ETSValue;
-    //   document.getElementById('passwordHelp').className = 'text-danger';
-    // }
-  // }
-
-  /** 
-  * Here we create and render the PDF
-  */
-
-  // var doc = new jsPDF();
-
-  // content = document.getElementById('section');
-  // var specialElementHandlers = {
-  //       '#editor': function (element, renderer) {
-  //           return true;
-  //       }
-  //   };
-
-  //   doc.fromHTML(content).html(), 15, 15, {
-  //       'width': 170,
-  //           'elementHandlers': specialElementHandlers
-  //   };
-  //   doc.save('sample-file.pdf');
-
-  // var height = 60;
-  // var interligne = 15;
+  if (
+    rs !== "" &&
+    siret !== "" &&
+    mail !== "" &&
+    tel !== "" &&
+    montant !== ""
+  ) {
+  if (parseInt(montant) > 0 && parseInt(montant) <= parseFloat(montantDu)) {
+    var height = 60;
+    var interligne = 15;
 
   // Default export is a4 paper, portrait, using milimeters for units
   
-  // function getImgFromUrl(logo_url, callback) {
-  //   var img = new Image();
-  //   img.src = logo_url;
-  //   img.onload = function () {
-  //       callback(img);
-  //   };
-  // }
+    function getImgFromUrl(logo_url, callback) {
+      var img = new Image();
+      img.src = logo_url;
+      img.onload = function () {
+          callback(img);
+      };
+    }
 
-  // function generatePDF(img){
-  //   var doc = new jsPDF();
-  //   doc.addImage(img, 'JPEG', 1, 2, 210, 30);
-  //   doc.setFontSize(24);
-  //   doc.text('Nom de l\'entreprise :', 105, height, "center");
-  //   doc.text(rs, 105, height += interligne, "center");
-  //   doc.text('Numéro de Siret :', 105, height += 20, "center");
-  //   doc.text(siret, 105, height += interligne, "center");
-  //   doc.setFontSize(20);
-  //   doc.text('Le montant à verser est de : ' + montant, 105, height += 50, "center");
-  //   doc.text("A verser par : " + moyenDePaiement, 105, height += interligne, "center");
-  //   doc.text("A l'adresse", 105, height += interligne, "center");
-  //   doc.setFontSize(18);
-  //   doc.text("Etablissement ", 105, height += interligne, "center");
-  //   doc.text("Adresse numéro de la rue", 105, height += interligne, "center");
-  //   doc.text("Ville et code postal", 105, height += interligne, "center");
-  //   doc.save("a4.pdf");
-  // }
+    function generatePDF(img){
+      var doc = new jsPDF();
+      doc.addImage(img, 'JPEG', 1, 2, 210, 30);
+      doc.setFontSize(24);
+      doc.text('Nom de l\'entreprise :', 50, height, "center");
+      doc.text(rs, 150, height, "center");
+      doc.text('N° Siret :', 40, height += 20, "center");
+      doc.text(siret, 150, height, "center");
+      doc.setFontSize(20);
+      doc.text('Montant à verser est de : ' + montant + ' €', 50, height += 30, "center");
+      doc.text("A verser par : " + moyenDePaiement, 40, height += interligne, "center");
+      doc.text("A l'adresse", 105, height += interligne, "center");
+      doc.setFontSize(18);
+      doc.text("Etablissement ", 105, height += interligne, "center");
+      doc.text("Adresse numéro de la rue", 105, height += interligne, "center");
+      doc.text("Ville et code postal", 105, height += interligne, "center");
+      doc.save("monBordereauDeVersement.pdf");
+    }
 
-  // var logo_url = "http://localhost:8888/Simulateur_CSM/src/assets/img/bannerCSM.jpeg";
-  // getImgFromUrl(logo_url, function (img) {
-  //     generatePDF(img);
-  // });  
+    var logo_url = "http://localhost:8888/Simulateur_CSM/src/assets/img/bannerCSM.jpeg";
+    getImgFromUrl(logo_url, function (img) {
+        generatePDF(img);
+    });
+    } else {
+      document.getElementById('errorForm').innerHTML = 'Erreur dans le formulaire, merci de vérifier les champs';
+      document.getElementById('errorForm').className = 'col-md-12 text-danger';
+      document.getElementById('errorSomme').innerHTML = 'La somme doit être comprise entre 0 et ' + montantDu;
+      document.getElementById('errorSomme').className = 'col-md-12 text-danger text-center';
+    }
+  }
+   /** 
+  * Here we create and render the PDF
+  */
+
+
+//   var url = "/Simulateur_CSM/src/TestBook.xlsx";
+
+  // /* set up async GET request */
+  // var req = new XMLHttpRequest();
+  // req.open("GET", url, true);
+  // req.responseType = "arraybuffer";
+
+  // req.onload = function(e) {
+  //   var data = new Uint8Array(req.response);
+  //   var wb = XLSX.read(data, { type: "array" });
+  //   var ws = wb.Sheets.Sheet1;
+  //   XLSX.utils.sheet_add_aoa(ws, [["new data", 1, 2, 3]], { origin: -1 });
+
+  //   XLSX.writeFile(wb, "new.xlsx");
+
+  //   // var htmlstr = XLSX.write(wb,{sheet:"Sheet1", type:'binary',bookType:'html'});
+  //   // $('#wrapper')[0].innerHTML += htmlstr;
+  // };
+
+  // req.send();
 });
 
 
